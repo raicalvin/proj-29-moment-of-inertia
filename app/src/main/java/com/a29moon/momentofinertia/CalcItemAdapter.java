5d874f9102a6e3bb857dbc7a4c231c5157c29184
Calvin Rai <raicalvin@gmail.com>
2019-01-30 09:48:24 -0800

    Implemented RecyclerView successfully with initial content for title and description.

--- app/src/main/java/com/a29moon/momentofinertia/CalcItemAdapter.java
+++ app/src/main/java/com/a29moon/momentofinertia/CalcItemAdapter.java
@@ -1,14 +1,40 @@
 package com.a29moon.momentofinertia;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CalcItemAdapter extends RecyclerView.Adapter<CalcItemAdapter.CalcItemViewHolder> {
 
     private CalcItem[] mDataSet;
 
+    @Override
+    public void onBindViewHolder(CalcItemViewHolder viewHolder, int position) {
+        viewHolder.mTitleTextView.setText(mDataSet[position].getNameOfCalculator());
+        viewHolder.mDescriptionTextView.setText(mDataSet[position].getDescriptionOfCalculator());
+    }
+
     public static class CalcItemViewHolder extends RecyclerView.ViewHolder {
 
+        // This is the template you created for a single list item
+        public LinearLayout mLinearLayout;
+
+        // The individual views inside the linear layout
+        public ImageView mThumbnailView;
+        public TextView mTitleTextView;
+        public TextView mDescriptionTextView;
+
+        public CalcItemViewHolder(LinearLayout ll) {
+            super(ll);
+            mLinearLayout = ll; // not sure if this is needed
+            mThumbnailView = (ImageView) ll.findViewById(R.id.calc_item_thumbnail_view);
+            mTitleTextView = (TextView) ll.findViewById(R.id.calc_item_title_text_view);
+            mDescriptionTextView = (TextView) ll.findViewById(R.id.calc_item_description_text_view);
+        }
     }
 
     public CalcItemAdapter(CalcItem[] dataSet) {
@@ -17,7 +43,9 @@ public class CalcItemAdapter extends RecyclerView.Adapter<CalcItemAdapter.CalcIt
 
     @Override
     public CalcItemAdapter.CalcItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.calc_item_layout, parent, false); // Create the  linear layout view
+        CalcItemViewHolder vh = new CalcItemViewHolder(v); // pass it into the view holder
+        return vh;
     }
 
     @Override
